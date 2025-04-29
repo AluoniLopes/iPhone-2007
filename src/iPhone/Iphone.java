35137c652e2c0677b0a2526f06c48a967baf1ba5
Aluoní <105504582+AluoniLopes@users.noreply.github.com>
2025-04-29 16:05:12 -0300

    criado a classe principal iPhone para composiçao

---
+++ src/iPhone/Iphone.java
@@ -0,0 +1,12 @@
+package iPhone;
+
+import iPhone.modules.AparelhoTelefonico;
+import iPhone.modules.NavegadorInternet;
+import iPhone.modules.ReprodutorMusical;
+
+public class Iphone {
+    public ReprodutorMusical musica = new ReprodutorMusical();
+    public AparelhoTelefonico telefone = new AparelhoTelefonico();
+    public NavegadorInternet internet = new NavegadorInternet();
+
+}
